053441834777c5878ac16bea7b228cab511e8b6e
marchinel <naila.shafa41@ui.ac.id>
2025-09-24 00:45:40 +0700

    Menyelesaikan tugas 4

--- main/models.py
+++ main/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     CATEGORY_CHOICES = [
         ('jersey', 'Jersey'),
         ('sepatu', 'Sepatu'),
--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from main.views import show_main, create_product, show_product, show_xml, show_json
+from main.views import register, login_user, logout_user
 
 app_name = 'main'
 
@@ -9,4 +10,7 @@ urlpatterns = [
     path('product/<str:id>/', show_product, name='show_product'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
